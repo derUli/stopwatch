7443aad17a4a172bead5b79c03f325d1f0f0cf76
Ulrich Schmidt <u.schmidt@velian.de>
2017-03-28 12:56:37 +0200

    Chronme.com als Alternative

--- src/content/modules/stopwatch/templates/embed.php
+++ src/content/modules/stopwatch/templates/embed.php
@@ -1,2 +1,1 @@
-<iframe src="http://ipadstopwatch.com/embed.html" frameborder="0"
-	scrolling="no" width="391" class="stopwatch" height="140"></iframe>
+<?php echo Template::executeModuleTemplate("stopwatch", "services/ipadstopwatch"); ?>
---
+++ src/content/modules/stopwatch/templates/services/chronme.php
@@ -0,0 +1,4 @@
+<div style="width:164px;"><!-- Chronme Online Stopwatch widget. --><iframe src="http://www.chronme.com/index_mini.html" frameborder="0" marginwidth="0px" marginheight="0px "width="165" height="110" scrolling="no"></iframe>
+<div style="font-size:0.8em; margin-top:-15px; font-family:Verdana, Arial, Helvetica, sans-serif;text-align:center;"><a style="text-decoration:none;color:#CCCCCC;" href="http://online-stopwatch.chronme.com" title="Online Stopwatch full version">online stopwatch</a></div>
+</div>
+<br />
